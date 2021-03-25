93e758cfb160614e046639d4e238d1b8e21db77a
chienlm-1082 <le.minh.chien@sun-asterisk.com>
2021-03-25 23:12:26 +0700

    draw face to the frame

--- detect_track.py
+++ detect_track.py
@@ -11,8 +11,10 @@ from tracking.deep_sort.tracker import Tracker
 from utils import check_position, draw, mouse_event
 from face_recog.face_detection.face_detector import get_detector, face_detect
 import imutils
+import numpy as np
 
 COLOR = [tuple([rd.randint(0, 255)]*3), tuple([rd.randint(0, 255)]*3), tuple([rd.randint(0, 255)]*3)]
+COLOR = [(255, 255, 0), (204, 0, 153), (26, 209, 255), (71, 107, 107)]
 print(COLOR)
 cap = cv2.VideoCapture("videos/01.mp4")
 
@@ -51,7 +53,7 @@ while cap.isOpened():
     start = time.time()
     classes, scores, bb_list = detector.detect(frame=frame, confidence_threshold=0.4, nms_threshold=0.4)
     s1 = time.time()
-    print("Detection time: {}".format(s1 - start))
+    # print("Detection time: {}".format(s1 - start))
 
     # after detection we will check position of object
     bb_check = dict()
@@ -67,7 +69,7 @@ while cap.isOpened():
     tracker.predict()
     tracker.update(detections)
     s2 = time.time()
-    print("Tracking time: {}".format(s2-s1))
+    # print("Tracking time: {}".format(s2-s1))
     for track in tracker.tracks:
         if not track.is_confirmed() or track.time_since_update > 1:
             continue
@@ -75,15 +77,21 @@ while cap.isOpened():
         cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),
                       COLOR[track.track_id%3],2)
         obj_img = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        scale_factor = 450 / obj_img.shape[1]
         obj_img = imutils.resize(obj_img, width=450)
         s3 = time.time()
         face_boxes = face_detect(obj_img, face_detector)
-        print("Face detection time: {}".format(time.time() - s3))
+        # print("Face detection time: {}".format(time.time() - s3))
         for box in face_boxes:
+          box = np.array(box) * (1/scale_factor)
           x_min, x_max, y_min, y_max = box
-          cv2.rectangle(obj_img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 3)
-
-        cv2.imshow("Obj ID {}".format(str(track.track_id)), obj_img)
+          x_min_real = int(x_min + bbox[0])
+          y_min_real = int(y_min + bbox[1])
+          x_max_real = int(x_max + bbox[0])
+          y_max_real = int(y_max + bbox[1])
+          # cv2.rectangle(obj_img, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 3)
+          cv2.rectangle(frame, (x_min_real, y_min_real), (x_max_real, y_max_real), (0, 255, 0), 2)
+        # cv2.imshow("Obj ID {}".format(str(track.track_id)), obj_img)
 
         # cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 1, (0, 0, 255), 2)
 
